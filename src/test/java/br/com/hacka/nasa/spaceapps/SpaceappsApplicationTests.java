963bb4575d080546bebb10edf8a6cf058a11fa10
root <root@F003126.finnet.corp>
2019-10-20 03:01:15 -0300

    Initial Commit

---
+++ src/test/java/br/com/hacka/nasa/spaceapps/SpaceappsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hacka.nasa.spaceapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceappsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
